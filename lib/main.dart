2dc835009b6b4b76ff8192a9128f98ee3ff682fc
Raj Bipinchandra Valand <rj452246@dal.ca>
2022-04-20 00:28:06 -0300

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    double days = 30;
+    String name = "Hello Dude";
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello Morning Star"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
